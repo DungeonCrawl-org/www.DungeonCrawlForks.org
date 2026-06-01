560c0723c302fc714a77406986398639e28807c9
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-01 13:24:34 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -12,7 +12,7 @@
 
 	echo '<h3>' . _("<b>BACKGROUND:</b> Dungeon Crawl (DC) is the game with many forks to choose from.") . "</h3>";
 	echo '<ul><li>' . _("And this is the NEW website with all the details about DC forks currently being developed.") . "</li>";
- 	echo	 '<li><a href="https://www.dungeoncrawlcentral.org/online_servers.html" target="_blank">' . _("Play online") . "</a>" . _(" on a Webtiles server.") . "</li>";
+ 	echo	 '<li><a href="online_servers" target="_blank">' . _("Play online") . "</a>" . _(" on a Webtiles server.") . "</li>";
 	echo     '<li><a href="https://www.dungeoncrawlcentral.org/download_forks.html" target="_blank">' . _("Download") . "</a>" . _(" an offline version of one of the DC forks.") . "</li>";
 	echo     "<li>" . _("Join our ") . '<a href="https://discord.gg/gMnE5JFcB7" target="_blank">' . _("Dungeon Crawl community") . "</a>". _(" discord server; the only place to discuss all DC forks!") . "</li>";
 	echo     "<li>" . _("Post to Reddit in ") . '<a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank">' . _("r/dungeoncrawl") . "</a></li></ul>";
