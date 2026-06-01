1242d13f009efe0b8c9b678a58a99b8099866da6
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-01 15:13:56 -0400

    Update neck_dropdown_menus.html.php

--- content/parts/neck_dropdown_menus.html.php
+++ content/parts/neck_dropdown_menus.html.php
@@ -22,6 +22,6 @@
 		<button class="dropbtn">Donate!</button>
 		<div class="dropdown-content" style="right:0;">
 			Please donate for the cost of our CCO.CrawlCosplay.org WebTiles server at:<br>
-			<a href="https://patreon.com/rogga" target="_blank"><b style="color:yellow">RoGGa's Patreon</b></a>
+			<a href="https://patreon.com/rogga" target="_blank"><b style="color:yellow">Patreon</b></a>
 		</div>
 	</div>
