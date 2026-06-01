7adba104e71abfb85de8c3d4d60ec89a879b5684
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-01 16:13:05 -0400

    Update closest_server.html.php

--- content/pages/closest_server.html.php
+++ content/pages/closest_server.html.php
@@ -1,7 +1,7 @@
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min.js"></script>
 
-<script type="text/javascript" src="//dungeoncrawlforks.org/js/closest_server.js"></script>
+<script type="text/javascript" src="/js/closest_server.js"></script>
 
 <h2 style="color:orange">Closest Dungeon Crawl server</h2>
 <p>Select the option to allow this page to determine your location</p>
