d967bf252f90c82599dcd25260fd2313b9b76fcd
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-01 13:23:06 -0400

    Create online_servers.html.php

---
+++ content/pages/online_servers.html.php
@@ -0,0 +1,106 @@
+<h2 style="color:orange"><center>Online Dungeon Crawl WebTiles servers</center></h2>
+
+<h3>Click the botton to determine the closest Dungeon Crawl server from your location:</h3>
+<center><button class="btn btn-success" onclick=" window.open('https://www.DungeonCrawlCentral.org/closest_server.html','_blank')"> <b>Find closest Dungeon Crawl WebTiles server</b> </button></center>
+
+<h3>Or select a server manually depending on its forks installed:</h3>
+<table>
+<tr><td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="http://cco.crawlcosplay.org" target="_blank"><b>CCO*</b></a>:     Toronto, Canada - <a href="http://cco.crawlcosplay.org" target="_blank">WebTiles</a> & Console (coming soon)</summary>
+            <ul><li>bcrawl</li>
+                <li>bcadren</li>
+                <li>stoatsoup</li>
+                <li>Stone Soup v0.11 to v0.34 and unstable Trunk</li>
+            </ul>
+</details></td>
+
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl.nemelex.cards" target="_blank"><b>CNC</b></a>:     Gyeonggi, Korea - <a href="https://crawl.nemelex.cards" target="_blank">WebTiles</a> & Console</summary>
+            <ul><li>bcrawl</li>
+                <li>bcadren</li>
+                <li>stoatsoup</li>
+                <li>Stone Soup v0.30 to v0.34 and unstable Trunk</li>
+            </ul>
+</details></td>
+
+</tr>
+
+<tr><td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl.akrasiac.org:8443" target="_blank"><b>CAO</b></a>: Arizona, USA - <a href="https://crawl.akrasiac.org:8443" target="_blank">Webtiles</a> & Console</summary>
+            <ul><li>Stone Soup v0.11 to v0.34 and unstable Trunk</li></ul>
+</details></td>
+
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                 <a href="https://cbro.berotato.org" target="_blank"><b>CBRO</b></a>:      Ohio, USA - <a href="https://cbro.berotato.org" target="_blank">WebTiles</a> & Console</summary>
+            <ul><li>Stone Soup v0.24 to v0.34 and unstable Trunk</li></ul>
+</details></td>
+</tr>
+
+<tr><td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl.dcss.io" target="_blank"><b>CDI</b></a>:           New York, USA - <a href="https://crawl.dcss.io" target="_blank">WebTiles</a> & Console</summary>
+            <ul><li>Stone Soup v0.30 to v0.34 and unstable Trunk</li></ul>
+</details></td>
+
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl.develz.org" target="_blank"><b>CDO</b></a>:        Nuremberg, Germany - Console ONLY</summary>
+            <ul><li>Stone Soup v0.2x to v0.34 and unstable Trunk</li></ul>
+</details></td>
+</tr>
+
+<tr><td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl.xtahua.com" target="_blank"><b>CXC</b></a>:        Paris, France - <a href="https://crawl.xtahua.com" target="_blank">WebTiles</a> & Console</summary>
+            <ul><li>bcrawl</li>
+                <li>bcadren</li>
+                <li>stoatsoup</li>
+                <li>Stone Soup v0.25 to v0.34 and unstable Trunk</li></ul>
+</details></td>
+
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://underhound.eu:8080" target="_blank"><b>CUE</b></a>:      Falkenstein, Germany - <a href="https://underhound.eu:8080" target="_blank">Webtiles</a> & Console</summary>
+            <ul><li>bcrawl</li>
+                <li>Stone Soup v0.10 to v0.34 and unstable Trunk</li>
+            </ul>
+</details></td>
+</tr>
+
+<tr>
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="http://lazy-life.ddo.jp:8080/#lobby" target="_blank"><b>LLD</b></a>:   Tokyo, Japan - <a href="http://lazy-life.ddo.jp:8080/#lobby" target="_blank">WebTiles ONLY</a></summary>
+            <ul><li>Stone Soup v0.34 only</li></ul>
+</details></td>
+
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl.project357.org" target="_blank"><b>CPO</b></a>:    Sydney, Australia - <a href="https://crawl.project357.org" target="_blank">WebTiles ONLY</a></summary>
+            <ul><li>stoatsoup</li>
+                <li>Stone Soup v0.25 to v0.34 and unstable Trunk</li>
+            </ul>
+</details></td>
+</tr>
+
+<tr>
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl.roguelikes.gg/#lobby" target="_blank"><b>CRG</b></a>:   Oregon, USA - <a href="https://crawl.roguelikes.gg/#lobby" target="_blank">WebTiles</a> & Console</summary>
+            <ul><li>Stone Soup v0.30 to v0.34 and unstable Trunk</li></ul>
+</details></td>
+
+<td><details open style="display:inline-block;">
+        <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
+                <a href="https://crawl-br.roguelikes.gg/#lobby" target="_blank"><b>CBRG</b></a>:    Brazil - <a href="https://crawl-br.roguelikes.gg/#lobby" target="_blank">WebTiles</a> & Console</summary></summary>
+            <ul><li>Stone Soup v0.30 to v0.34 and unstable Trunk</li>
+            </ul>
+</details></td>
+</tr>
+</table>
+<p>*CCO isn't tracked by the DCSS bots or stats pages.</p>
+		<br />
