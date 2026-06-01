2f42da6b201f82c775a249e89ac3077827a7349f
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-01 16:53:41 -0400

    Update neck_dropdown_menus.html.php

--- content/parts/neck_dropdown_menus.html.php
+++ content/parts/neck_dropdown_menus.html.php
@@ -4,8 +4,8 @@
 			<a href="/">Dungeon Crawl Forks homepage</a>
 			<a href="https://discord.gg/gMnE5JFcB7" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl community discord server</a>
 			<a href="/online_servers.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online on a Dungeon Crawl server</a>
-			<a href="/downloadforks.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download a Dungeon Crawl fork for offline play</a>
-			<a href="/forksdetails.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; View Dungeon Crawl forks details</a>
+			<a href="/download_forks.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download a Dungeon Crawl fork for offline play</a>
+			<a href="/forks_details.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; View Dungeon Crawl forks details</a>
 			<a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl subreddit</a>
 			<a href="https://www.crawlcosplay.org/" target="_blank">Crawl Cosplay (CC) website</a>
 			<a href="https://discord.gg/pW7nqC8Wu3" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Crawl Cosplay 2.0 community discord server</a>
