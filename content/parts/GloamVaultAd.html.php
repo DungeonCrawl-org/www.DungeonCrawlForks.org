0f1b9d4650556d8e12969012b46a1e14b5cb1d6e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-02 20:22:51 -0400

    Update GloamVaultAd.html.php

--- content/parts/GloamVaultAd.html.php
+++ content/parts/GloamVaultAd.html.php
@@ -10,7 +10,7 @@
 <div class="floating-banner">
 	<h4>Website Sponsor</h4>
 	<a href="https://store.steampowered.com/app/3460840/Gloamvault/?utm_source=dcss_site&utm_medium=referral&utm_campaign=dcss_site" target="_blank">
-        <img src="/img/ads/gloamvault-rightbanner4.gif" alt="GloatVault image" title="This site is partially funded by the game GloamVault. Please visit their entry on Steam by clicking the image abouve to get them to increase their funding for this website.">
+        <img src="/img/ads/gloamvault-rightbanner4.gif" alt="GloatVault image" title="This site is partially funded by the game GloamVault. Please visit their entry on Steam by clicking the image above to get them to increase their funding for this website.">
     </a>
 	<h4>the game with DCSS sprites</h4>
     <!-- Optional Close Button -->
