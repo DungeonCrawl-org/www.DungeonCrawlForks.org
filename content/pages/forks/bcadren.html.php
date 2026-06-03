5817bac5fc280b762988508f30cf39836d0a256b
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-03 16:24:12 -0400

    Add BcadrenCrawl HTML page
    This file contains the HTML structure and content for the BcadrenCrawl page, including styles, navigation, and links to resources.

---
+++ content/pages/forks/bcadren.html.php
@@ -0,0 +1,154 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Dungeon Crawl Central</title>
+    <link rel="icon" href="/img/feat-dg_entrance.png" type="image/png">
+    <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
+    <link rel="stylesheet" href="/css/dungeoncrawl.css?v=<?=time()?>">
+    <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
+
+<style>
+
+#dlTable {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#dlTable td, #dlTable th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#dlTable tr:nth-child(even){background-color: Dark Gray}
+
+#dltable tr:hover {background-color: #ddd;}
+
+#dlTable th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: drak gray;
+  color: white;
+}
+
+
+div.score-sidebar-cca {
+    float: right;
+    clear: both;
+    text-align: right;
+    width: 575px;
+    padding: 0 2em 5em;
+}
+	
+.centerTable { 
+    margin: 0px auto; 
+    border: none;
+}
+	
+h1	{
+	font-size: 48px;
+	text-align: center;
+}
+
+.dropbtn {
+  background-color: #7D623C;
+  color: white;
+  padding: 12px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  right: 0;
+  background-color: #7D623C;
+  min-width: 500px;
+  box-shadow: 0px 8px 12px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 12px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #000000;}
+.dropdown:hover .dropdown-content {display: block;}
+.dropdown:hover .dropbtn {background-color: #7D623C;}
+
+</style>
+
+</head>
+<body class="page_back">
+<div class="page_floor">
+
+<div class="page information">
+	<div class="dropdown" style="float:left;">
+		<button class="dropbtn">Menu</button>
+		<div class="dropdown-content" style="left:0;">
+			<a href="/">Dungeon Crawl Central website</a>
+			<a href="https://discord.gg/gMnE5JFcB7" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl community discord server</a>
+			<a href="/online_servers.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Play online on a Dungeon Crawl server</a>
+			<a href="/download.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Download a Dungeon Crawl fork for offline play</a>
+			<a href="/forks.html"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; View Dungeon Crawl forks details</a>
+			<a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Dungeon Crawl subreddit</a>
+			<a href="https://www.crawlcosplay.org/">Crawl Cosplay (CC) website</a>
+			<a href="https://discord.gg/pW7nqC8Wu3" target="_blank"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&#8627; Crawl Cosplay 2.0 community discord server</a>
+		</div>
+	</div>
+
+	<div class="dropdown" style="float:right;">
+		<button class="dropbtn">Donate!</button>
+		<div class="dropdown-content" style="right:0;">
+			Please donate for the cost of the CCO.CrawlCosplay.Org webtiles server at:<br>
+			<a href="https://patreon.com/rogga" target="_blank"><b style="color:yellow">Patreon</b></a>
+		</div>
+	</div>    <div class="toplinks"><br></div>
+    <div class="page_content">
+        <div class="heading">
+            <h1><img src="/img/feat-dg_entrance.png"> <a href="https://www.DungeonCrawlCentral.org">www.DungeonCrawlCentral.org</a> <img src="/img/feat-dg_entrance.png"></h1>
+	          <div class="fineprint"><center>Come chat with us in our new <a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18"></a></center></div>
+        </div>
+	<br>
+        <img src="/img/HR-right.png"><br>
+      
+<h1 style="color:orange">BcadrenCrawl: Boulder Brew</h1>
+
+<h2>BCadrenCrawl: a Dungeon Crawl fork based on Stone Soup fork (DCSS) v0.2x</h2>
+
+<p>GitHub repo: <a href="https://github.com/Bcadren/crawl" target="_blank">github.com/Bcadren/crawl</a></p>
+
+<p>Download at: <a href="https://github.com/Bcadren/crawl/releases" target="_blank">github.com/Bcadren/crawl/releases</a></p>
+
+<p>Stats pages for BCrawl: <a href="https://bcadren.montres.org.uk/overview.html" target="_blank">bcadren.montres.org.uk/overview.html</a></p>
+
+<p>List of high-level changes: <a href="https://github.com/Bcadren/crawl/blob/bCrawl/README.md" target="_blank">https://github.com/Bcadren/crawl/blob/bCrawl/README.md</a></p>
+
+<p>Full 34 page description of BCadrenCrawl: <a href="https://docs.google.com/document/d/1gNrFq3TNuaUoSYYwBHJG4bprF4zrJwzvipujZh6DFHI/edit?pli=1&tab=t.0" target="_blank">@ docs.google.com</a></p>
+
+<p>Donate/Support @: <a href="https://www.patreon.com/Bcadren" target="_blank">Patreon</a></p>
+
+	    	<br />
+		<img src="/img/HR-right.png"><br />
+		<br />
+		<footer>Copyleft  |  <a href="https://github.com/DungeonCrawl-org/www.DungeonCrawlCentral.org/issues" target="_blank">Send bug report or feature request</a>
+		<p style="text-align:right;">You are too beserk; please calm down first!</p></footer>
+		</div>
+	</div>
+</div>
+</div>
+<div id="page_footer">&nbsp;</div>
+</body>
+</html>
