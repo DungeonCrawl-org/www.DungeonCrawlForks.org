59987208b7d3f412131e6ce19e7a57fbec9fa50e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-03 13:02:22 -0400

    Update head.html.php

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -61,7 +61,7 @@ h1	{
   position: absolute;
   right: 0;
   background-color: #7D623C;
-  min-width: 600px;
+  min-width: 450px;
   white-space: nowrap;
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
   z-index: 1;
