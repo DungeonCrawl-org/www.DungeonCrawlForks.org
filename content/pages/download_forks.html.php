2ff8ac614180bd2457ec2dcc12e6aeadc8b4f99d
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-03 16:20:46 -0400

    Change heading from h2 to h1 and update links

--- content/pages/download_forks.html.php
+++ content/pages/download_forks.html.php
@@ -1,10 +1,10 @@
 <?php
 #$this->layout = 'dungeoncrawl'; 
 ?>
-<h2>Download Dungeon Crawl forks</h2>
+<h1>Download Dungeon Crawl forks</h1>
 <ul>
-    <li><h4><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download the BCadren fork</a></h4></li>
-    <li><h4><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download the BCrawl fork</a></h4></li>
-    <li><h4><a href="https://github.com/damerell/crawl/releases" target="_blank">Download the Stoat Soup fork</a></h4></li>
-    <li><h4><a href="/dungeoncrawl/download_dcss">Download the Stone Soup (DCSS) fork</a></h4></li>
+    <li><h4><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h4></li>
+    <li><h4><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h4></li>
+    <li><h4><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h4></li>
+    <li><h4><a href="/dungeoncrawl/download_dcss">Download Stone Soup (DCSS)</a></h4></li>
 </ul>
