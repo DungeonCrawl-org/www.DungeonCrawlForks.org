5102cf01ab33fe82171299db94d4af8948530db8
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-03 10:29:19 -0400

    Update head.html.php

--- content/layouts/default/head.html.php
+++ content/layouts/default/head.html.php
@@ -7,7 +7,7 @@
     <title><?=$page_title ?? 'Dungeon Crawl Forks'?></title>
     <link rel="icon" href="/img/Rift.png" type="image/png">
     <!-- <link rel="stylesheet" 	href="/css/reset.css"> -->
-    <link rel="stylesheet" href="/css/cosplay.css?v=<?=time()?>">
+    <link rel="stylesheet" href="/css/default.css?v=<?=time()?>">
     <!-- <link rel="stylesheet" href="https://crawl.develz.org/tournament/0.23/tourney-score.css"> -->
 
 <style>
