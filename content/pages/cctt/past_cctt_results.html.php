4a67a8ff4da1fdaf1e7841dbc184fb05fb552666
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-04 05:03:13 -0400

    delete subfolders ccc cca cctt

--- content/pages/cctt/past_cctt_results.html.php
+++
@@ -1,8 +0,0 @@
-<?php $this->layout = 'cctt'; ?>
-
-<h2>Previous Crawl Cosplay Trunk Tournament (CCTT) results</h2>
-
-<ul><li><a href="/cctt/tresults.html?set=32">CCTT#1</a> June 2024</li>
-    <li><a href="/cctt/tresults.html?set=33">CCTT#2</a> March 2025</li>
-    <li><a href="/cctt/tresults.html?set=34">CCTT#3</a> December 2025</li>
-</ul>
