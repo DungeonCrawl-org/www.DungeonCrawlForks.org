6a0d068091a760bcea4c7dec27fc2253b6a0a2aa
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-04 08:02:15 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -24,7 +24,7 @@
 
 	echo "<h2>" . _("The latest NEWS") . "</h2>";
 	echo "<ul><li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccsdt" target="_blank">' . _("Crawl Cosplay Sudden Death Tournament") . '</a>' . _(" (CCSDT#3) is currently ") . '<b>' . _("LIVE") . '</b>' . 
-	echo '<dotlottie-wc src="https://lottie.host/8264036f-c526-4820-afa7-7ede2247bd13/Bby6r9p7r8.lottie" style="width: 30px;height: 30px" autoplay loop></dotlottie-wc>';
+	     '<dotlottie-wc src="https://lottie.host/8264036f-c526-4820-afa7-7ede2247bd13/Bby6r9p7r8.lottie" style="width: 30px;height: 30px" autoplay loop></dotlottie-wc>';
 
 	echo     "<li>" . _("Before the end of July, the following will likely be happening:") . "</li>";
 	echo     "<ul><li>" . _("In order to reduce costs, the webtiles server CCO (cco.crawlcosplay.org) will be moved to a European server.") . "</li>";
