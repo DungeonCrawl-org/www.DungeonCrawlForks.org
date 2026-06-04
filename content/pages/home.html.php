0c61cc99dd72743c8679afaba9e76b5316cf00fc
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-04 07:59:30 -0400

    Fix HTML output for news section in home page

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -23,7 +23,7 @@
 	echo     "<li>" . _("Post to Reddit in ") . '<a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank">' . _("r/dungeoncrawl") . "</a></li></ul>";
 
 	echo "<h2>" . _("The latest NEWS") . "</h2>";
-	echo "<ul><li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccsdt" target="_blank">' . _("Crawl Cosplay Sudden Death Tournament") . '</a>' . _(" (CCSDT#3) is currently ") . '<b>' . _("LIVE") . </b>; 
+	echo "<ul><li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccsdt" target="_blank">' . _("Crawl Cosplay Sudden Death Tournament") . '</a>' . _(" (CCSDT#3) is currently ") . '<b>' . _("LIVE") . '</b>'; 
 	echo <dotlottie-wc src="https://lottie.host/8264036f-c526-4820-afa7-7ede2247bd13/Bby6r9p7r8.lottie" style="width: 30px;height: 30px" autoplay loop></dotlottie-wc>;
 
 <?php
