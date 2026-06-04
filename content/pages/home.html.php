aa61a189a8675c254838f07f46057249437937c4
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-04 04:05:31 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -17,9 +17,9 @@
 
 	echo "<h2>" . _("The latest NEWS") . "</h2>";
 	echo "<ul><li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccsdt" target="_blank">' . _("Crawl Cosplay Sudden Death Tournament") . '</a>' . _(" (CCSDT#3) is currently LIVE");
-	echo     "<li>" . _("Before the end of July, the following will likely be happening:") . "</li></ul>";
-	echo             _(" 1. The webtiles server CCO (cco.crawlcosplay.org) will be moved to a European server.");
-	echo     		 _(" 2. A new Webtiles server will be setup in Ontario, Canada to be called: DCF (dcf.dungeoncrawlforks.org");
+	echo     "<li>" . _("Before the end of July, the following will likely be happening:") . "</li>";
+	echo     "<ul><li>" . _("The webtiles server CCO (cco.crawlcosplay.org) will be moved to a European server.") . "</li>";
+	echo     	 "<li>" . _("A new Webtiles server will be setup in Ontario, Canada to be called: DCF (dcf.dungeoncrawlforks.org") . "</li></ul></ul>";
 	
 	echo "<h2>" . _("Want to chat?") . "</h2>";
 	echo "<p>" . _("Come chat with us on our Dungeon Crawl Crawl community")
