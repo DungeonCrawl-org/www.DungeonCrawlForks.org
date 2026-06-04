a99d8676676b186669b9dde78043a6fc37b60caf
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-04 06:28:22 -0400

    Update forks_details.html.php

--- content/pages/forks_details.html.php
+++ content/pages/forks_details.html.php
@@ -10,14 +10,12 @@
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/bcadren"><b>BCadren</b></a></summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
-                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li></ul>
 </details></p>
 <details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/bcrawl"><b>BCrawl</b></a></summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
-                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
                 <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:      Falkenstein, Germany</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li></ul>
 </details></p>
@@ -25,7 +23,6 @@
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/stoat_soup"><b>Stoat Soup</b></a></summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
-                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:         Seattle, USA</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
                 <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:    Sydney, Australia</li></ul>
 </details></p>
@@ -33,12 +30,12 @@
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/stone_soup"><b>Stone Soup</b></a> (DCSS)</summary>
             <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:         Toronto, Canada</li>
-                <li><a href="https://crawl.yiuf.org/" target="_blank">CYO</a>:             Seattle, USA</li>
                 <li><a href="https://crawl.akrasiac.org:8443" target="_blank">CAO</a>:     Arizona, USA</li>
                 <li><a href="https://cbro.berotato.org" target="_blank">CBRO</a>:          Ohio, USA</li>
                 <li><a href="https://crawl.dcss.io" target="_blank">CDI</a>:               New York, USA</li>
                 <li><a href="https://crawl.xtahua.com" target="_blank">CXC</a>:            Paris, France</li>
                 <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:          Falkenstein, Germany</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:         Gyeonggi, Korea</li>
-                <li><a href="http://lazy-life.ddo.jp:8080/#lobby" target="_blank">LLD</a>: Tokyo, Japan</li></ul>
+                <li><a href="http://lazy-life.ddo.jp:8080/" target="_blank">LLD</a>:       Tokyo, Japan</li></ul>
+                <li><a href="https://crawl-br.roguelikes.gg/" target="_blank">CBRG</a>:    Brazil</li></ul>
 </details></p>
