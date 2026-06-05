d170e8a5f7992c3fd0c8b77e72aae3ee35dfce68
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-05 08:19:58 -0400

    Update header section with new image links

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -4,7 +4,10 @@
     	<div class="toplinks"><br></div>
     	<div class="page_content">
         	<div class="heading">
-            		<h1><center><img src="/img/banner-left.gif" width="256" height="256"><a href="/"><img src="/img/DCF-logo-homepage.gif" width="256" height="256"></a><img src="/img/banner-right.gif" width="256" height="256"></center></h1>
+            		<h1><center><a href="forks_details"><img src="/img/banner-left.gif" width="256" height="256"></a>
+								<a href="/"><img src="/img/DCF-logo-homepage.gif" width="256" height="256"></a>
+								<a href="forks_details"><img src="/img/banner-right.gif" width="256" height="256"></a>
+						</center></h1>
 			<div class="fineprint"><center>Come chat with us in our <a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
         	</div>
 		<div class="fineprint" align="right">
