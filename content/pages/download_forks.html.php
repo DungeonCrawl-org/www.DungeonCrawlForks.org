770791f5c44037f7d80247d51762c08317581190
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 12:29:03 -0400

    Refactor video section and download links layout
    Updated video container and download links for Dungeon Crawl forks.

--- content/pages/download_forks.html.php
+++ content/pages/download_forks.html.php
@@ -3,16 +3,15 @@
 ?>
 <h2>Download Dungeon Crawl forks</h2>
 
-<div  class="video-container">
-    <video autoplay loop playsinline>
-      <source src="/img/arena_animations/arena-aizulVSagnes-01.mp4" type="video/mp4">
+<div class="clearfix">
+     <video class="floated-video" controls autoplay loop playsinline>
+            <source src="/img/arena_animations/arena-aizulVSagnes-01.mp4" type="video/mp4">
       Your browser does not support the video tag to show an animation of 2 Uniques fighting in the Arena.
     </video>
+    <ul>
+        <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
+        <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
+        <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
+        <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h4></li>
+    </ul>
 </div>
-
-<ul>
-    <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
-    <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
-    <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
-    <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h4></li>
-</ul>
