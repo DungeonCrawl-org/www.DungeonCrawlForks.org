0d77a6a2c47530cca213694c08cfe25340ff778c
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 10:46:33 -0400

    Update download_forks.html.php

--- content/pages/download_forks.html.php
+++ content/pages/download_forks.html.php
@@ -4,8 +4,8 @@
 <h1>Download Dungeon Crawl forks</h1>
 <img src="/img/arena_gifs/Arena01.gif" class="arena">
 <ul>
-    <li><h4><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h4></li>
-    <li><h4><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h4></li>
-    <li><h4><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h4></li>
-    <li><h4><a href="/dungeoncrawl/download_dcss">Download Stone Soup (DCSS)</a></h4></li>
+    <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
+    <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
+    <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
+    <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h4></li>
 </ul>
