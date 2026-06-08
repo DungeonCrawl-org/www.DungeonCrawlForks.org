6b85315e89cd8c71a6f5d567502d933adf2bee31
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 11:28:03 -0400

    Update download_forks.html.php

--- content/pages/download_forks.html.php
+++ content/pages/download_forks.html.php
@@ -1,14 +1,14 @@
 <?php
 #$this->layout = 'dungeoncrawl'; 
 ?>
-<h1>Download Dungeon Crawl forks</h1>
-<video autoplay loop muted playsinline controls>
-  <source src="/img/arena_animations/arena01.mp4" type="video/mp4">
-  Your browser does not support the video tag.
-</video>
+<h2>Download Dungeon Crawl forks</h2>
 <ul>
     <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
     <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
     <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
     <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h4></li>
 </ul>
+<video autoplay loop muted playsinline controls>
+  <source src="/img/arena_animations/arena01.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
