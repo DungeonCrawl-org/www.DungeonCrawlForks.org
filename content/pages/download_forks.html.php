5f6ca92bcbf5b02415a01af39c5e809419d8a846
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 13:08:33 -0400

    Update download_forks.html.php

--- content/pages/download_forks.html.php
+++ content/pages/download_forks.html.php
@@ -2,16 +2,15 @@
 #$this->layout = 'dungeoncrawl'; 
 ?>
 <h2>Download Dungeon Crawl forks</h2>
-<ul>
-    <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
-    <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
-    <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
-    <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h4></li>
-</ul>
-
-<div style="display: flex; justify-content: center; align-items: center;">
-    <video autoplay loop playsinline>
-      <source src="/img/arena_animations/arena02-3vs2.mp4.mp4" type="video/mp4">
-      Your browser does not support the video tag to show an animation of 2 Uniques fighting in the Arena.
+<div class="clearfix">
+    <video class="floated-video" controls autoplay loop playsinline>
+           <source src="/img/arena_animations/arena02-3vs2.mp4.mp4" type="video/mp4">
+            Your browser does not support the video tag to show an animation of 2 Uniques fighting in the Arena.
     </video>
+    <ul>
+        <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
+        <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
+        <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
+        <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h3></li>
+    </ul>
 </div>
