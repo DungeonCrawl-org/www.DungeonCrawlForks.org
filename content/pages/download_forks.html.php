08827842a2ef6dc01b03418f486d5cd27497b3be
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 12:23:12 -0400

    Fix HTML structure and duplicate download links

--- content/pages/download_forks.html.php
+++ content/pages/download_forks.html.php
@@ -2,16 +2,17 @@
 #$this->layout = 'dungeoncrawl'; 
 ?>
 <h2>Download Dungeon Crawl forks</h2>
+
+<div  class="video-container"">
+    <video autoplay loop playsinline>
+      <source src="/img/arena_animations/arena-aizulVSagnes-01.mp4" type="video/mp4">
+      Your browser does not support the video tag to show an animation of 2 Uniques fighting in the Arena.
+    </video>
+</div>
+
 <ul>
     <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
     <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
     <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
     <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h4></li>
 </ul>
-
-<div  class="video-container" style="display: flex; justify-content: center; align-items: center;">
-    <video autoplay loop playsinline>
-      <source src="/img/arena_animations/arena-aizulVSagnes-01.mp4" type="video/mp4">
-      Your browser does not support the video tag to show an animation of 2 Uniques fighting in the Arena.
-    </video>
-</div>
