f0b1fa7a47238db346201bba225e7dd13742294e
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 13:16:21 -0400

    Add unique visitor counter to home page

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -30,3 +30,6 @@
 	
 	echo "<p>" . _("Happy Crawling!") . "</p>";
 ?>
+<p><center>Unique Visitors since May 2026</center></p>
+<p><center><script type="text/javascript" src="https://counter.websiteout.com/js/2/5/-1/1"></script></center></p>
+<br>
