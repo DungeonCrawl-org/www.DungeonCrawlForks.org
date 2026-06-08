41eb039ef086c1fda7c9e21c32fe395e80a3a916
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 15:42:41 -0400

    Change unique visitors text to header format

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -30,6 +30,6 @@
 	
 	echo "<p>" . _("Happy Crawling!") . "</p>";
 ?>
-<p><center>Unique Visitors since June 2026</center></p>
+<h3><b><center>Unique Visitors since June 2026</center></b></h3>
 <p><center><script type="text/javascript" src="https://counter.websiteout.com/js/2/5/-1/1"></script></center></p>
 <br>
