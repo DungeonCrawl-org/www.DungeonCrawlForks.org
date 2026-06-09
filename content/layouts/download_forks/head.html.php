a30a8577819136d0fd615aa9bf01a60315392d13
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-09 07:18:24 -0400

    Update alt text for image in head.html.php

--- content/layouts/download_forks/head.html.php
+++ content/layouts/download_forks/head.html.php
@@ -149,7 +149,7 @@ h1	{
         $meta += [
             'width' => 256,
             'height' => 256,
-            'alt' => "Crawl Cosplay server",
+            'alt' => "Download a Dungeon Crawl Fork",
             'type' => "image/png",
         ];
         if (strpos($meta['filename'], 'https') === 0) {
