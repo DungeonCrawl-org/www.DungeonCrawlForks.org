2ac65d96556eeebfce0b2f11fedcd368e65fad20
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-08 21:08:32 -0400

    misc

--- content/layouts/download_forks/neck.html.php
+++ content/layouts/download_forks/neck.html.php
@@ -4,7 +4,10 @@
     	<div class="toplinks"><br></div>
     	<div class="page_content">
         	<div class="heading">
-            		<h1><center><img src="/img/DCF-logo-homepage.gif" width="256" height="256"></center></h1>
+            		<h1><center><a href="forks_details"><img src="/img/banner-left.gif" width="256" height="256"></a>
+								<a href="/"><img src="/img/DCF-logo-homepage.gif" width="256" height="256" title="Image created by our own Red Eye."></a>
+								<a href="forks_details"><img src="/img/banner-right.gif" width="256" height="256"></a>
+						</center></h1>
 			<div class="fineprint"><center>Come chat with us in our <a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
         	</div>
 		<div class="fineprint" align="right">
@@ -14,7 +17,8 @@
     				| Challenges <a href="/admin/challenges/list">List</a> <a href="/admin/challenges/add">New</a>
 	        		| <a href="/admin/logout">Logout</a>
 			<?php else : ?>
-          			<a href="/backoffice">Admin</a>
+<!--          			<a href="/backoffice">Admin</a> 
+	--> <br>
 			<?php endif; ?>
 		</div>
         	<img src="/img/HR-right.png"><br />
