413ad67e0f534f0d2beb2fff088f8d990d223bf6
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-10 18:06:23 -0400

    Fix formatting for CCFT#2 announcement

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -21,7 +21,7 @@
 	echo     "<li>" . _("Before the end of July, the following will likely be happening:") . "</li>";
 	echo     "<ul><li>" . _("In order to reduce costs, the webtiles server CCO (cco.crawlcosplay.org) will be moved to a European server.") . "</li>";
 	echo     	 "<li>" . _("A new Webtiles server will be setup in Ontario, Canada to be called: DCF (dcf.dungeoncrawlforks.org).") . "</li></ul>";
-	echo 	 "<li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccft" target="_blank">' . _("Crawl Cosplay Forks Tournament") . '</a>(CCFT#2)' . _(" will likely start in August.") . "</li></ul>";
+	echo 	 "<li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccft" target="_blank">' . _("Crawl Cosplay Forks Tournament") . '</a> (CCFT#2) ' . _("will likely start in August.") . "</li></ul>";
 	
 	echo "<h2>" . _("Want to chat?") . "</h2>";
 	echo "<p>" . _("Come chat with us on our")
