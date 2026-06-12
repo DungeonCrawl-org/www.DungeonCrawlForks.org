f0a55b8324c3e51279b368df277607e985fbbae5
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-12 14:22:14 -0400

    Add links and image to BCrawl page

--- content/pages/forks/bcrawl.html.php
+++ content/pages/forks/bcrawl.html.php
@@ -6,8 +6,12 @@
 
 <h2>BCrawl: a Dungeon Crawl fork based on Stone Soup fork (DCSS) v0.2x</h2>
 
+<img src="/img/forks/bcrawl/xtuhua.png" style="float:right">
+
 <p>GitHub repo: <a href="https://github.com/b-crawl/bcrawl" target="_blank">github.com/b-crawl/bcrawl</a></p>
 
 <p>Download at: <a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">github.com/b-crawl/bcrawl/releases</a></p>
+
+<p>View the <a href="https://b-crawl.github.io/" target="_blank">About BCrawl</a> page.</p>
 
 <p>Stats pages for BCrawl: <a href="https://bcrawl.montres.org.uk/overview.html" target="_blank">bcrawl.montres.org.uk/overview.html</a></p>
