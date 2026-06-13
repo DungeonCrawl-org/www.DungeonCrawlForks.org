2197330d38b38ea7ec47dc0b9df5750f1df51b15
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-13 11:46:44 -0400

    title_banner for dcss

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -8,7 +8,7 @@
 								<a href="/forks/stoat_soup"><img src="/img/title_banner/banner-left-stoatsoup.jpg"  height="256" title="click to view the details about the Stoat Soup fork"></a>
 								<a href="/"><img src="/img/title_banner/DCF-logo-homepage.gif" width="256" height="256" title="Image created by our own Red Eye."></a>
 								<a href="/forks/bcadren"><img src="/img/title_banner/banner-right-bcadren.jpg"  height="256" title="click to view the details about the BCadren fork"></a>
-								<a href="/forks/stone_soup"><img src="/img/banner-right-dcss.gif" height="256" title="click to view the details about the Stone Soup (aka DCSS) fork"></a>
+								<a href="/forks/stone_soup"><img src="/img/title_banner/banner-right-dcss.gif" height="256" title="click to view the details about the Stone Soup (aka DCSS) fork"></a>
 						</center></h1>
 			<div class="fineprint"><center>Come chat with us in our <a href="https://discord.gg/gMnE5JFcB7" target="_blank">Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a></center></div>
         	</div>
