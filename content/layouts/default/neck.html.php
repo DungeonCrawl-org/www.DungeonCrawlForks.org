c27dc3388473dfd85ea988cb41aff3e26f90d404
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-13 14:08:57 -0400

    update necks

--- content/layouts/default/neck.html.php
+++ content/layouts/default/neck.html.php
@@ -6,9 +6,9 @@
         	<div class="heading">
             		<h1><center><a href="/forks/chili"><img src="/img/title_banner/coming_soon-DCChile.gif"  height="256" title="click to view the details about the DCChili fork"></a>
 								<a href="/forks/bcrawl"><img src="/img/title_banner/banner-left-bcrawl.jpg"  height="256" title="click to view the details about the BCrawl fork"></a>
-								<a href="/forks/stoat_soup"><img src="/img/title_banner/banner-left-stoatsoup.jpg"  height="256" title="click to view the details about the Stoat Soup fork"></a>
-								<a href="/"><img src="/img/title_banner/DCF-logo-homepage.gif" width="256" height="256" title="Image created by our own Red Eye."></a>
 								<a href="/forks/bcadren"><img src="/img/title_banner/banner-right-bcadren.jpg"  height="256" title="click to view the details about the BCadren fork"></a>
+								<a href="/"><img src="/img/title_banner/DCF-logo-homepage.gif" width="256" height="256" title="Image created by our own Red Eye."></a>
+								<a href="/forks/stoat_soup"><img src="/img/title_banner/banner-left-stoatsoup.jpg"  height="256" title="click to view the details about the Stoat Soup fork"></a>
 								<a href="/forks/stone_soup"><img src="/img/title_banner/banner-right-dcss.jpg" height="256" title="click to view the details about the Stone Soup (aka DCSS) fork"></a>
 								<a href="/forks/chili"><img src="/img/title_banner/coming_soon-DCChile.gif"  height="256" title="click to view the details about the DCChili fork"></a>
 						</center></h1>
