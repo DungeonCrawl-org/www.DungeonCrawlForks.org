b4c99cfd22005eee83ffa5b4dec05422933383c3
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-13 13:08:55 -0400

    create DCChili page

---
+++ content/pages/forks/dcchilli.htlm.php
@@ -0,0 +1,17 @@
+<?php 
+    $this->layout = 'forks_details'; 
+?>
+
+<h1 style="color:orange">Dungeon Crawl Chili</h1>
+
+<h2>DCChili: a Dungeon Crawl fork based on the Stone Soup (DCSS) fork v0.34 with significant content from BCrawl and BCadren Crawl</h2>
+
+<img src="/img/forks/dcchili/your-logo-here.jpg" style="float:right" title="Your logo here! Contact us to have your logo featured here.">
+
+<p>GitHub repo: <a href="https://github.com/DungeonCrawl-org/DungeonCrawlChili" target="_blank">github.com/DungeonCrawl-org/DungeonCrawlChili</a></p>
+
+<p>Download at: Not available yet</p>
+
+<p>View the <a href="/forks/DCChili/aboutDCChili" target="_blank">About DungeonCrawlChili</a> page.</p>
+
+<p>Stats pages for DCChili: Coming in July or Augustmost likely</p>
