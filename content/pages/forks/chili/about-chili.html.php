9e26499e33b834cd986b1716bb8b4ff8a88134a0
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-13 13:33:10 -0400

    about chili

---
+++ content/pages/forks/chili/about-chili.html.php
@@ -0,0 +1,19 @@
+<?php 
+    $this->layout = 'forks_details'; 
+?>
+
+<h1 style="color:orange">About Dungeon Crawl Chili</h1>
+
+<h2>DC Chili is a Dungeon Crawl fork based on the Stone Soup (DCSS) fork v0.34 with significant content from BCrawl and BCadren Crawl</h2>
+
+<img src="/img/forks/dcchili/your-logo-here.jpg" style="float:right" title="Your logo here! Contact us to have your logo featured here.">
+
+<p>Founding members:
+<ul><li>RoGGa (Project Manager)</li>
+    <li>BCadren (Lead Developer)</li>
+    <li>Red Eye (Junior Developer and Graphics Artist)</li>
+</ul>
+</p>
+
+<p><b>Devs and graphics artists wanted!</b> If you are interested in helping out with the development of DCChili, please join our <a href="https://discord.gg/gMnE5JFcB7" target="_blank">
+   Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a> and let RoGGa know!</p>
