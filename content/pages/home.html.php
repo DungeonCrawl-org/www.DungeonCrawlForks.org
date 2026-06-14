831bde8798fc6210882672cf0e554c371a9cb362
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-14 06:28:18 -0400

    Update home.html.php

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -19,8 +19,8 @@
 	echo "<h2>" . _("The latest NEWS") . "</h2>";
 	echo "<ul><li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccsdt" target="_blank">' . _("Crawl Cosplay Sudden Death Tournament") . '</a>' . _(" (CCSDT#3) is currently ") . '<b>' . _("LIVE") . '</b>&nbsp<img src="/img/misc/arrow-flashing-left.gif" width="30">';
 	echo     "<li>" . _("A new fork was just launched named: ") . "<a href='/forks/chili'><b>Dungeon Crawl Chili</b></a></li>";
-	echo     "<li>" . _("Before the end of July, the following will likely be happening:") . "</li>";
 	echo     "<li>" . _("The CNC 2nd Anniversary Tournament will be held for two weeks starting at 00:00 on the 18th (KST):") . "<a href='https://refracta.github.io/nemelex.cards/cnc-2nd-anniversary-tournament/details.html' target='_blank'>Details</a></li>";
+	echo     "<li>" . _("Before the end of July, the following will likely be happening:") . "</li>";
 	echo     "<ol><li>" . _("In order to reduce costs, the webtiles server CCO (cco.crawlcosplay.org) will be moved to a European server.") . "</li>";
 	echo     	 "<li>" . _("A new Webtiles server will be setup in Ontario, Canada to be called: DCF (dcf.dungeoncrawlforks.org).") . "</li></ol>";
 	echo 	 "<li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccft" target="_blank">' . _("Crawl Cosplay Forks Tournament") . '</a> (CCFT#2) ' . _("will likely start in August.") . "</li></ul>";
