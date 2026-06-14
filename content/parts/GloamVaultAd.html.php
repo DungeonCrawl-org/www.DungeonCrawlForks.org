7102337440a99ee041e59f5fca3ead17ef863421
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-14 08:12:39 -0400

    Update GloamVaultAd.html.php

--- content/parts/GloamVaultAd.html.php
+++ content/parts/GloamVaultAd.html.php
@@ -8,13 +8,12 @@
 
 <!-- Floating Banner Container -->
 <div class="floating-banner">
-	<h4>Website Sponsor</h4>
-	<a href="https://store.steampowered.com/app/3460840/Gloamvault/?utm_source=dcss_site&utm_medium=referral&utm_campaign=dcss_site" target="_blank">
+    <button id="close-banner">✕</button>
+    <h4>Website Sponsor</h4>
+	  <a href="https://store.steampowered.com/app/3460840/Gloamvault/?utm_source=dcss_site&utm_medium=referral&utm_campaign=dcss_site" target="_blank">
         <img src="/img/ads/gloamvault-rightbanner4.gif" alt="GloatVault image" title="This site is partially funded by the game GloamVault. Please visit their entry on Steam by clicking the image above to get them to increase their funding for this website.">
     </a>
-	<h4>a game with DCSS sprites</h4>
-    <!-- Optional Close Button -->
-    <button id="close-banner" onclick="this.parentElement.style.display='none'" class="close-btn">&times;</button>
+	  <h4>a game with DCSS sprites</h4>
 </div>
 
 <script>
