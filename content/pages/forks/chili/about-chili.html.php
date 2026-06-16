be092673f8b3d3d80bf749181dca30f7509cfc72
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-16 10:34:49 -0400

    Update launch goal date for chili fork

--- content/pages/forks/chili/about-chili.html.php
+++ content/pages/forks/chili/about-chili.html.php
@@ -10,7 +10,7 @@
 
 ...with significant content imported from BCadren Crawl.<br><br>
 
-<p>The fork was launched in: <b>June 2026</b></p> with the goal of having a working alpha entity on the CCO Webtiles server in July 2026.
+<p>The fork was launched in: <b>June 2026</b></p> with the goal of having a working beta version on the CCO Webtiles server in time for the CCForksT in August 2026.
 <p><b>Founding members</b>:
 <ul><li>RoGGa: Project Manager</li>
     <li>BCadren: Lead Developer</li>
