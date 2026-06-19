831be57d4d0d5c03c20c68067cd54a464d6943c9
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-19 14:35:20 -0400

    Fix HTML output for news section in home page

--- content/pages/home.html.php
+++ content/pages/home.html.php
@@ -17,8 +17,7 @@
 	echo     "<li>" . _("Read or Post to subReddit: ") . '<a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank">' . _("r/dungeoncrawl") . "</a></li></ul>";
 
 	echo "<h2>" . _("The latest NEWS") . "</h2>";
-	echo "<ul><li>" . _("The ") . '<a href="https://www.crawlcosplay.org/ccsdt" target="_blank">' . _("Crawl Cosplay Sudden Death Tournament") . '</a>' . _(" (CCSDT#3) is currently ") . '<b>' . _("LIVE") . '</b>&nbsp<img src="/img/misc/arrow-flashing-left.gif" width="30">';
-	echo     "<li>" . _("A new fork was just launched named: ") . "<a href='/forks/chili'><b>Dungeon Crawl Chili</b></a></li>";
+	echo "<ul><li>" . _("A new fork was just launched named: ") . "<a href='/forks/chili'>Dungeon Crawl Chili</a></li>";
 	echo     "<li>" . _("The CNC 2nd Anniversary Tournament will be held for two weeks starting at 00:00 on the 18th (KST): ") . "<a href='https://refracta.github.io/nemelex.cards/cnc-2nd-anniversary-tournament/details.html' target='_blank'>Website's Details</a></li>";
 	echo     "<li>" . _("Before the end of July, the following will likely be happening:") . "</li>";
 	echo     "<ol><li>" . _("In order to reduce costs, the webtiles server CCO (cco.crawlcosplay.org) will be moved to a European server.") . "</li>";
