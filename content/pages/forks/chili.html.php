9221b8cc1c3bf9830a75e5ec40817d2c28ca901b
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-27 08:57:06 -0400

    Update content and structure in chili.html.php

--- content/pages/forks/chili.html.php
+++ content/pages/forks/chili.html.php
@@ -9,26 +9,7 @@
 <a href="https://dungeoncrawlchili.org/" target="_blank"><img src="/img/forks/dcchili/DungeonCrawlChiliOrg-YourLogoHere.gif" height=256 style="float:right" title="Your logo here! Contact us to have your logo featured here."></a>
 <p>...with significant content to be imported from BCadren Crawl.</p>
 
-<p>The fork was launched in: <b>June 2026</b></p> with the goal of having a working beta version on the CCO Webtiles server in time for the CCForksT in August 2026.
-<h2>Founding members</h2>
-<ul><li>RoGGa: Project Manager</li>
-    <li>BCadren: Lead Developer</li>
-    <li>Red Eye: Junior Developer and Graphics Artist</li>
-    <li>srcrip:  Junior Developer</li>
-</ul>
+<h4>Website: <a href="https://dungeoncrawlchili.org/" target="_blank">www.DungeonCrawlChili.org</h4></a>
+    <p>The fork was launched in: <b>June 2026</b></p> with the goal of having a working beta version on the CCO Webtiles server in time for the CCForksT in August 2026.
 
 <p><b>GitHub repository</b>: <a href="https://github.com/DungeonCrawl-org/DungeonCrawlChili" target="_blank">github.com/DungeonCrawl-org/DungeonCrawlChili</a></p>
-
-<p>Download at: Not available yet</p>
-
-<p>Chili Wiki available @ <a href="http://wiki.dungeoncrawlchili.org" target="_blank">wiki.dungeoncrawlchili.org</a>
-
-<p>Stats pages for DC Chili: Coming in July or August most likely</p>
-
-<h2>Devs and graphics artists wanted!</h2>
-<p>If you are interested in helping out with the development of DC Chili, please join our <a href="https://discord.gg/gMnE5JFcB7" target="_blank">
-Dungeon Crawl community discord server<img src="/img/discord_transparent_border.png" width="18" height="18" ></a> and let RoGGa know!</p>
-<p>The DC Chili fork is made up of a very small team right now and will need help in recruiting C programmers
-   ...so if you know anyone who wants to be an active dev for a new and exciting Dungeon Crawl fork...please send them our way!</p>
-<p>We're also looking for a few specialized devs to make big special vaults (in .des files) for the new branch
-   above the Dungeon to be called (most likely): the Corrupted Kingdom (K:1-3)</p>
