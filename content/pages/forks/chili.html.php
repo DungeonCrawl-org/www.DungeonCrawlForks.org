859c0fee9e6b67d6669dd7e73b0826d50abe7fe7
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-06-27 09:00:14 -0400

    Refactor website and GitHub repository links in chili.html.php
    Updated HTML structure for website and GitHub repository sections.

--- content/pages/forks/chili.html.php
+++ content/pages/forks/chili.html.php
@@ -9,7 +9,7 @@
 <a href="https://dungeoncrawlchili.org/" target="_blank"><img src="/img/forks/dcchili/DungeonCrawlChiliOrg-YourLogoHere.gif" height=256 style="float:right" title="Your logo here! Contact us to have your logo featured here."></a>
 <p>...with significant content to be imported from BCadren Crawl.</p>
 
-<h4>Website: <a href="https://dungeoncrawlchili.org/" target="_blank">www.DungeonCrawlChili.org</h4></a>
-    <p>The fork was launched in: <b>June 2026</b></p> with the goal of having a working beta version on the CCO Webtiles server in time for the CCForksT in August 2026.
+<h4>Website: <a href="https://dungeoncrawlchili.org/" target="_blank">www.DungeonCrawlChili.org</a></h4>
+<p>The fork was launched in: <b>June 2026</b></p> ...with the goal of having a working beta version on the CCO Webtiles server in time for the CCForksTournament starting at some point in August 2026.
 
-<p><b>GitHub repository</b>: <a href="https://github.com/DungeonCrawl-org/DungeonCrawlChili" target="_blank">github.com/DungeonCrawl-org/DungeonCrawlChili</a></p>
+<h4><b>GitHub repository</b>: <a href="https://github.com/DungeonCrawl-org/DungeonCrawlChili" target="_blank">github.com/DungeonCrawl-org/DungeonCrawlChili</a></h4>
