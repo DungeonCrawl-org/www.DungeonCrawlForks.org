3333a474321354bdb5edbc2c33b3664887edc090
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-04 18:30:18 -0400

    Update download links for Dungeon Crawl forks

--- webroot/download_forks.php
+++ webroot/download_forks.php
@@ -3,17 +3,18 @@
 ?>
 
 
-<h2>Download Dungeon Crawl forks</h2>
+<h2>Downloads for Dungeon Crawl forks</h2>
 <div class="clearfix">
     <video class="floated-video" controls autoplay loop playsinline>
            <source src="/img/arena_animations/arena02-3vs2.mp4" type="video/mp4">
             Your browser does not support the video tag to show an animation of 2 Uniques fighting in the Arena.
     </video>
     <ul>
-        <li><h3><a href="https://github.com/Bcadren/crawl/releases" target="_blank">Download BCadren</a></h3></li>
-        <li><h3><a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">Download BCrawl</a></h3></li>
-        <li><h3><a href="https://github.com/damerell/crawl/releases" target="_blank">Download Stoat Soup</a></h3></li>
-        <li><h3><a href="https://crawl.develz.org/download.htm" target="_blank">Download Stone Soup (DCSS)</a></h3></li>
+        <li><h3>- <a href="https://github.com/Bcadren/crawl/releases" target="_blank">BCadren</a></h3></li>
+        <li><h3>- <a href="https://github.com/b-crawl/bcrawl/releases" target="_blank">BCrawl</a></h3></li>
+        <li><h3>- <a href="https://dungeoncrawlchili.org/downloads.php" target="_blank">Dungeon Crawl Chili</a></h3></li>
+        <li><h3>- <a href="https://github.com/damerell/crawl/releases" target="_blank">Stoat Soup</a></h3></li>
+        <li><h3>- <a href="https://crawl.develz.org/download.htm" target="_blank">Stone Soup (DCSS)</a></h3></li>
     </ul>
 </div>
 <?php require 'views/footer.php'; ?>
