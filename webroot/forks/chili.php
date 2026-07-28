40fa96d4034db9d4a73d95e6bdeb5e96eb43adfa
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-28 17:27:46 -0400

    Modify launch date and add webtiles server info
    Updated launch date and added testing server link.

--- webroot/forks/chili.php
+++ webroot/forks/chili.php
@@ -10,11 +10,11 @@ include __DIR__ . '/../views/neck.php';
 
 <h2>DC Chili is a Dungeon Crawl fork based on the Stone Soup (DCSS) fork v0.35 trunk</h2>
 
-<a href="https://dungeoncrawlchili.org/" target="_blank"><img src="/img/forks/dcchili/DungeonCrawlChiliOrg.gif" height=256 style="float:right" title="Your logo here! Contact us to have your logo featured here."></a>
+<a href="https://dungeoncrawlchili.org/" target="_blank"><img src="/img/forks/dcchili/DungeonCrawlChiliOrg.gif" height=256 style="float:right"></a>
 <p>...with significant content to be imported from BCadren Crawl.</p>
 
 <h4>Website: <a href="https://dungeoncrawlchili.org/" target="_blank">www.DungeonCrawlChili.org</a></h4>
-<p>The fork was launched in: <b>June 2026</b></p> ...with the goal of having a working beta version on the CCO Webtiles server in time for the CCForksTournament starting at some point in August 2026.
-
+<p>The fork was launched in: <b>June 2026</b></p> ...with the goal of having a working beta version  in time for the CCForksTournament starting at some point in August 2026.
+<p>You can test it out on the new webtiles server: <a href="https://dcf.DungeonCrawlForks.org/" target="_blank">dcf.DungeonCrawlForks.org</a></p>
 <h4><b>GitHub repository</b>: <a href="https://github.com/DungeonCrawl-org/DungeonCrawlChili" target="_blank">github.com/DungeonCrawl-org/DungeonCrawlChili</a></h4>
 <?php include __DIR__ . '/../views/footer.php'; ?>
