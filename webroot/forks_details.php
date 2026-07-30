6c0f073c7a3286829b9349fe7cc0bec76476ae61
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-07-30 04:33:19 -0400

    Revise fork details and links in forks_details.php
    Updated links and locations for various forks.

--- webroot/forks_details.php
+++ webroot/forks_details.php
@@ -10,19 +10,21 @@
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/chili"><b>Dungeon Crawl Chili</b></a> - NEW!</summary>
             <ul><li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCF</a>:     Ontario, Canada</li>
-                <li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada - COMING SOON!</li></ul>
+                <li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCT</a>:     Germany</li></ul>
 </details></p>
 <details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
-                <a href="/forks/bcadren"><b>BCadren</b></a></summary>
-            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
-                <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
-                <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:    Sydney, Australia</li></ul>
+                <a href="/forks/bcadren"><b>BCadren Crawl</b></a></summary>
+            <ul><li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCF</a>:     Ontario, Canada</li>
+                <li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCT</a>:     Germany</li>
+                <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:          Gyeonggi, Korea</li>
+                <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:         Sydney, Australia</li></ul>
 </details></p>
 <details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/bcrawl"><b>BCrawl</b></a></summary>
-            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
+            <ul><li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCF</a>:     Ontario, Canada</li>
+                <li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCT</a>:     Germany</li>
                 <li><a href="https://underhound.eu:8080" target="_blank">CUE</a>:      Falkenstein, Germany</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
                 <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:    Sydney, Australia</li></ul>
@@ -30,14 +32,16 @@
 <details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/stoat_soup"><b>Stoat Soup</b></a></summary>
-            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:     Toronto, Canada</li>
+            <ul><li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCF</a>:     Ontario, Canada</li>
+                <li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCT</a>:     Germany</li>
                 <li><a href="https://crawl.nemelex.cards" target="_blank">CNC</a>:     Gyeonggi, Korea</li>
                 <li><a href="https://crawl.project357.org" target="_blank">CPO</a>:    Sydney, Australia</li></ul>
 </details></p>
 <details open style="display:inline-block;">
         <summary style="cursor:pointer; -webkit-user-select:none; -moz-user-select:none; -ms-user-select:none; user-select:none;">
                 <a href="/forks/stone_soup"><b>Stone Soup</b></a> (DCSS)</summary>
-            <ul><li><a href="http://cco.crawlcosplay.org" target="_blank">CCO</a>:         Toronto, Canada</li>
+            <ul><li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCF</a>:    Ontario, Canada</li>
+                <li><a href="http://dcf.dungeoncrawlforks.org" target="_blank">DCT</a>:    Germany</li>
                 <li><a href="https://crawl.akrasiac.org:8443" target="_blank">CAO</a>:     Arizona, USA</li>
                 <li><a href="https://cbro.berotato.org" target="_blank">CBRO</a>:          Ohio, USA</li>
                 <li><a href="https://crawl.dcss.io" target="_blank">CDI</a>:               New York, USA</li>
