de916cc09b3efe3219dd9075bb7f45e0db7e8b76
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-08-07 10:13:02 -0400

    Fix typo in header for forks details page

--- webroot/forks_details.php
+++ webroot/forks_details.php
@@ -2,7 +2,7 @@
         include 'views/neck.php';
 ?>
 
-<h2 style="color:orange">Dungeon Crawl actively developped forks</h2>
+<h2 style="color:orange">Dungeon Crawl actively developed forks</h2>
 
 <h4>Click on the fork name's link to get more info on that fork.</h4>
 <br>
