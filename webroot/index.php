8ee5fe8b9b1e1d74dd4baea42b4839c97ad40414
RoGGa <119762169+RoGGa-69@users.noreply.github.com>
2026-09-12 07:57:32 -0400

    Update news section with new tournament details

--- webroot/index.php
+++ webroot/index.php
@@ -12,12 +12,13 @@ and this is the <b>NEW</b> website with all the details about DC forks currently
     <li>Read or Post to subReddit: <a href="https://www.reddit.com/r/dungeoncrawl/" target="_blank">r/dungeoncrawl</a></li></ul>
 
 <h2>The latest NEWS</h2>
-<ul><li>A new Dungeon Crawl fork was launched in June and it is named: <a href='https://www.dungeoncrawlforks.org/forks/chili' target='_blank'><b>Dungeon Crawl Chili</b></a> (DC Chili)</li>
+<ul><li>The <a href="www.crawlcosplay.org/cctt" target="_blank">Crawl Cosplay Trunk Tournament</a> (CCTT#4) starts on Friday September 18th at 00:00 UTC.</li>
+	<li>A new Dungeon Crawl fork was launched in June and it is named: <a href='https://www.dungeoncrawlforks.org/forks/chili' target='_blank'><b>Dungeon Crawl Chili</b></a> (DC Chili)</li>
 	<li>DC Chili is available to play online on the following new Webtiles servers:<br>
 		- <a href='https://dcf.dungeoncrawlforks.org/' target='_blank'><b>Dungeon Crawl Fajita (DCF)</b></a> located in Ontario Canada;<br>
 		- <a href='https://crawl.xtahua.com/' target='_blank'><b>crawl.xtahua.com (CXC)</b></a> located in France;<br>
 		- <a href='https://crawl.project357.org//' target='_blank'><b>crawl.project357.org/ (CPO)</b></a> located in Australia.</li>
-	<li>The <a href="www.crawlcosplay.org/cctt" target="_blank">Crawl Cosplay Trunk Tournament</a> (CCTT#4) will likely start at the end of September.</li></ul>
+</ul>
 
 <h2>Want to chat?</h2>
 	<img src="/img/title_banner/DungeonCrawlForks-chatGPTimage.gif" title="Image created by ChatGPT with guidance from RoGGa." style="float:right">
